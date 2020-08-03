62f01b42d801bb526c8e4414536ec6c53aa59216
xj-h <hanyuntaocn@163.com1456974798@qq.com>
2020-08-03 15:35:15 +0800

    22

--- src/config/index.js
+++ src/config/index.js
@@ -1,53 +1,97 @@
-// 一些全局的config配置
-const modeUrlObj = {
-  // 生产环境
-  production: {
-    name: 'prod',
-    orginUrl: window.location.origin,
-    domain: location.protocol + '//tkdoors.top/',
-    basePath: process.env.VUE_APP_BASEURL
+'use strict'
+// Template version: 1.2.6
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require('path')
+
+module.exports = {
+  dev: {
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {
+      '/apiproxy': {
+        target: 'http://tkdoors.top',
+        changeOrigin: true
+      },
+      '/dcloud': {
+        target: 'http://tkdoors.top',
+        changeOrigin: true
+      }
+    },
+
+    // Various Dev Server settings
+
+    // can be overwritten by process.env.HOST
+    // if you want dev by ip, please set host: '0.0.0.0'
+    host: 'localhost',
+    port: 9527, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: true,
+    errorOverlay: true,
+    notifyOnErrors: false,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+    // Use Eslint Loader?
+    // If true, your code will be linted during bundling and
+    // linting errors and warnings will be shown in the console.
+    useEslint: true,
+    // If true, eslint errors and warnings will also be shown in the error overlay
+    // in the browser.
+    showEslintErrorsInOverlay: false,
+
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'cheap-source-map',
+
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false
   },
-  // 预生产环境
-  preproduction: {
-    name: 'preprod',
-    orginUrl: window.location.origin,
-    domain: location.protocol + '//www.hanhuatong.com.cn/',
-    basePath: process.env.VUE_APP_BASEURL,
-    fmBankNo: '8808001000322821',
-    fmMerchantNo: '',
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL'
-  },
-  // 开发环境
-  development: {
-    name: 'dev',
-    orginUrl: window.location.origin,
-    domain: location.protocol + '//fangsit.91hanhua.com/',
-    basePath: '',
-    fmBankNo: '8808001000322821',
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL'
-  },
-  // 测试环境
-  sit: {
-    name: 'sit',
-    orginUrl: window.location.origin,
-    domain: location.protocol + '//fangsit.91hanhua.com/',
-    basePath: process.env.VUE_APP_BASEURL,
-    fmBankNo: '8808001000322821',
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL'
-  },
-  // 测试环境
-  uat: {
-    name: 'uat',
-    orginUrl: window.location.origin,
-    domain: location.protocol + '//fanguat.91hanhua.com/',
-    basePath: process.env.VUE_APP_BASEURL,
-    fmBankNo: '8808001000322821',
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL'
+
+  build: {
+    // Template for index.html
+    index: path.resolve(__dirname, '../dist/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+
+    /**
+     * You can set by youself according to actual condition
+     * You will need to set this if you plan to deploy your site under a sub path,
+     * for example GitHub pages. If you plan to deploy your site to https://foo.github.io/bar/,
+     * then assetsPublicPath should be set to "/bar/".
+     * In most cases please use '/' !!!
+     */
+    assetsPublicPath: '/platform/admin',
+
+    /**
+     * Source Maps
+     */
+    productionSourceMap: process.env.ENV_CONFIG !== 'production',
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: 'source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build:prod --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report || false,
+
+    // `npm run build:prod --generate_report`
+    generateAnalyzerReport: process.env.npm_config_generate_report || false
   }
 }
-const configObj = {
-  ...modeUrlObj[process.env.NODE_ENV],
-  serviceLink: 'http://uchat.im-cc.com/webchat_new/static/html/index.html?ht=7051'
-}
-// export default modeUrlObj[process.env.NODE_ENV]
-export default configObj
